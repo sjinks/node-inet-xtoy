b56a8ad1ac021affee14d7683ce61c10ad9aefc0
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-05-24 05:19:06 +0300

    fix: Use bindings

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const addon = require('./build/Release/inet_xtoy');
+const addon = require('bindings')('inet_xtoy');
 
 module.exports.inet_ntop = addon.inet_ntop;
 module.exports.inet_pton = addon.inet_pton;
